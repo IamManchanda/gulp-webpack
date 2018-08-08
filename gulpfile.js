4aec632fee9857bf67d86ad3a02abd4dd43ef1aa
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-08-08 21:12:51 +0530

    CSS specific: Gulp autoprefixer, plumber (errors) & sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,30 @@ const gulpUglifyEs = require('gulp-uglify-es').default;
 const gulpLiveReload = require('gulp-livereload');
 const gulpConcat = require('gulp-concat');
 const gulpMinifyCss = require('gulp-minify-css');
+const gulpAutoprefixer = require('gulp-autoprefixer');
+const gulpPlumber = require('gulp-plumber');
+const gulpSourcemaps = require('gulp-sourcemaps');
 
 const distPath = (file) => `./public/dist/${file}`;
 const srcPath = (file) => `./public/src/${file}/**/*.${file}`;
+const autoprefixConfig = {
+  // https://caniuse.com/#search=flex
+  browsers: [
+    'last 2 versions',
+    'ie >= 10',
+    'edge >= 12',
+    'firefox >= 28',
+    'chrome >= 21',
+    'safari >= 6.1',
+    'opera >= 12.1',
+    'ios >= 7',
+    'android >= 4.4',
+    'blackberry >= 10',
+    'operamobile >= 12.1',
+    'samsung >= 4',
+  ],
+  cascade: false,
+};
 
 // Images
 gulp.task('images', () => {
@@ -16,8 +37,15 @@ gulp.task('images', () => {
 gulp.task('styles', () => {
   return gulp
     .src(['./public/src/css/reset.css', srcPath('css')])
+    .pipe( gulpPlumber(function (err) {
+      console.error('Styles Task Error', err);
+      this.emit('end');
+    }) )
+    .pipe( gulpSourcemaps.init() )
+    .pipe( gulpAutoprefixer(autoprefixConfig) )
     .pipe( gulpConcat('styles.css') )
     .pipe( gulpMinifyCss() )
+    .pipe( gulpSourcemaps.write() )
     .pipe( gulp.dest(distPath('css')) )
     .pipe( gulpLiveReload() );
 });
