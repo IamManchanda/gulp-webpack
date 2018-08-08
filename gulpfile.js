142eaff90ffa93a48b85cdfddbd47d3179393741
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-08-09 01:31:51 +0530

    Image compression!

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,41 @@ const gulpPlumber = require('gulp-plumber');
 const gulpSourcemaps = require('gulp-sourcemaps');
 const gulpSass = require('gulp-sass');
 const gulpBabel = require('gulp-babel');
+const gulpImagemin = require('gulp-imagemin');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminJpegRecompress = require('imagemin-jpeg-recompress');
+
 const supportedBrowsers = require('./supported-browsers');
 
-const distPath = (file) => `./public/dist/${file}`;
-const srcPath = (file) => {
+const distPathForCode = (file) => `./public/dist/${file}`;
+const srcPathForCode = (file) => {
   if (file === 'scss') return `./public/src/${file}/styles.${file}`;
   return `./public/src/${file}/**/*.${file}`;
 };
+const srcPathForImages = './public/src/img/**/*.{png,jpeg,jpg,svg,gif}';
+const distPathForImages = './public/dist/img';
 
 const autoprefixConfig = { browsers: supportedBrowsers, cascade: false };
 const babelConfig = { targets: { browsers: supportedBrowsers } };
 
 // Images
 gulp.task('images', () => {
-  console.log('Starting Images task');
+  return gulp.src(srcPathForImages)
+    .pipe( gulpImagemin([
+      gulpImagemin.gifsicle(),
+      gulpImagemin.jpegtran(),
+      gulpImagemin.optipng(),
+      gulpImagemin.svgo(),
+      imageminPngquant(),
+      imageminJpegRecompress(),
+    ]) )
+    .pipe( gulp.dest(distPathForImages) );
 });
 
 // Styles (SCSS)
 gulp.task('styles', (cb) => {
   pump([
-    gulp.src(srcPath('scss')),
+    gulp.src(srcPathForCode('scss')),
     gulpPlumber(function (err) {
       console.error('Styles Task Error', err);
       this.emit('end');
@@ -36,7 +51,7 @@ gulp.task('styles', (cb) => {
     gulpAutoprefixer(autoprefixConfig),
     gulpSass({ outputStyle: 'compressed' }),
     gulpSourcemaps.write(),
-    gulp.dest(distPath('css')),
+    gulp.dest(distPathForCode('css')),
     gulpLiveReload(),
   ], cb);
 });
@@ -44,7 +59,7 @@ gulp.task('styles', (cb) => {
 // Scripts (JS)
 gulp.task('scripts', (cb) => {
   pump([
-    gulp.src(srcPath('js')),
+    gulp.src(srcPathForCode('js')),
     gulpPlumber(function (err) {
       console.error('Scripts Task Error', err);
       this.emit('end');
@@ -54,7 +69,7 @@ gulp.task('scripts', (cb) => {
     gulpConcat('scripts.js'),
     gulpUglify(),
     gulpSourcemaps.write(),
-    gulp.dest(distPath('js')),
+    gulp.dest(distPathForCode('js')),
     gulpLiveReload(),
   ], cb);
 });
@@ -67,6 +82,6 @@ gulp.task('default', ['images', 'styles', 'scripts'], () => {
 // Watch
 gulp.task('watch', ['default'], () => {
   gulpLiveReload.listen();
-  gulp.watch(srcPath('scss'), ['styles']);
-  gulp.watch(srcPath('js'), ['scripts']);
+  gulp.watch(srcPathForCode('scss'), ['styles']);
+  gulp.watch(srcPathForCode('js'), ['scripts']);
 });
