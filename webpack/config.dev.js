6d7f894a7763a51bb6a5de63c068d1ed0b4efdc9
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-08-13 01:02:35 +0530

    Add comment for webpack config!

--- webpack/config.dev.js
+++ webpack/config.dev.js
@@ -1,3 +1,6 @@
+// You can add other webpack configuration (plugins, loaders, etc).
+// Apart from ES6 Import/Export, Gulp was able to do all my other work so this file is mainly empty.
+
 module.exports = {
   mode: 'development',
   output: {
